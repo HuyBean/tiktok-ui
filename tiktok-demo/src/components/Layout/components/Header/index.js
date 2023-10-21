36d7d307dac2bafcbd748068bae84a737cc6dba2
HuyBean <nqhuy0711@gmail.com>
2023-10-21 22:56:35 +0700

    commit all

--- tiktok-demo/src/components/Layout/components/Header/index.js
+++ tiktok-demo/src/components/Layout/components/Header/index.js
@@ -1,10 +1,15 @@
-import classNames from 'classnames/classname'
+import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
 
 const cx = classNames.bind(styles)
 
 function Header() {
-    return <header className={cx('wrapper')}></header>
+    return <header className={cx('wrapper')}>
+        <div className={cx('inner')}>
+            {/*Logo*/}
+            {/*Search*/}
+        </div>
+    </header>
 }
 
 export default Header;
